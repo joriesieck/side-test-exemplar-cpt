ba991d8be1acd139e4c4d545467f389f33701a41
joriesieck <jsieck@globalcognition.org>
2019-05-30 12:45:13 -0400

    empty file to start plugin

---
+++ side-test-exemplars.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Exemplars Custom Post Type
+Version: 1.0
+Description: Creates a custom "Exemplars" post type
+Author: Jorie Sieck
+Author URI: https://my.thinkeracademy.com
+*/
+
